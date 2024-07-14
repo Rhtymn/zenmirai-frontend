374934920bc8ae1cf6b79f1ea7689232b4449560
Roihan <hroihan6@gmail.com>
2024-07-14 21:09:38 +0700

    add show spinner on loading functionality

--- src/components/ui/CButton.vue
+++ src/components/ui/CButton.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
+import CSpinner from '@/components/ui/CSpinner.vue'
 interface Props {
   type?: 'button' | 'submit' | 'reset' | undefined
+  isLoading?: boolean
 }
-defineProps<Props>()
+withDefaults(defineProps<Props>(), {
+  type: undefined,
+  isLoading: false
+})
 </script>
 
 <template>
   <button :type="type" class="bg-[#162D3A] text-white p-3 rounded-2xl">
-    <slot></slot>
+    <slot v-if="!isLoading"></slot>
+    <div v-else class="w-full flex justify-center">
+      <CSpinner />
+    </div>
   </button>
 </template>
