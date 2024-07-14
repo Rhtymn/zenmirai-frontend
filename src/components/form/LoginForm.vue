c0b5d11e00e8614bd855c75863af599fa1101b13
Roihan <hroihan6@gmail.com>
2024-07-14 21:45:52 +0700

    feat: implement login functionality without redirect yet

--- src/components/form/LoginForm.vue
+++ src/components/form/LoginForm.vue
@@ -1,22 +1,61 @@
 <script setup lang="ts">
 import CInput from '@/components/ui/CInput.vue'
 import CButton from '@/components/ui/CButton.vue'
+import FormNotification from '@/components/ui/FormNotification.vue'
 import { useForm } from 'vee-validate'
 import { loginSchema, type LoginFields } from '@/types/validators/auth'
+import { ref } from 'vue'
+import { type FormState } from '@/types'
+import { login } from '@/services/auth'
+import { BadRequest } from '@/exceptions/badRequest'
 
-const { defineField, errors, handleSubmit } = useForm<LoginFields>({
+const { defineField, errors, handleSubmit, setValues } = useForm<LoginFields>({
   validationSchema: loginSchema
+})
+const resetValues = () => {
+  setValues(
+    {
+      username: '',
+      password: ''
+    },
+    false
+  )
+}
+
+const formState = ref<FormState>({
+  error: '',
+  isError: false,
+  isSubmitting: false,
+  isSuccess: false
 })
 
 const [username, usernameAttrs] = defineField('username')
 const [password, passwordAttrs] = defineField('password')
 
-const onSubmit = handleSubmit((values: LoginFields) => {
-  console.log(values)
+const onSubmit = handleSubmit(async (values: LoginFields) => {
+  try {
+    formState.value.isSubmitting = true
+    const res = await login(values)
+    formState.value.isSuccess = true
+    resetValues()
+    alert(`Login success; token = ${res?.data?.token}`)
+  } catch (e) {
+    formState.value.isError = true
+    if (e instanceof BadRequest) {
+      formState.value.error = e.message
+    } else {
+      formState.value.error = 'server encounter error. try again later.'
+    }
+  } finally {
+    formState.value.isSubmitting = false
+  }
 })
 </script>
 
 <template>
+  <FormNotification :isShow="formState.isError" header="Login failed" type="failed">
+    {{ formState.error }}
+  </FormNotification>
   <form @submit="onSubmit" class="flex flex-col gap-y-4">
     <CInput
       id="username"
@@ -27,12 +66,15 @@ const onSubmit = handleSubmit((values: LoginFields) => {
     />
     <CInput
       id="password"
+      type="password"
       label="Password"
       v-model="password"
       v-bind="passwordAttrs"
       :errors="errors.password"
     />
-    <CButton>Sign In</CButton>
+    <CButton type="submit" class="cursor-pointer" :is-loading="formState.isSubmitting"
+      >Sign In</CButton
+    >
     <p class="self-end">
       Don't have an account?
       <RouterLink to="/register" class="text-blue-700 underline underline-offset-2 cursor-pointer"
