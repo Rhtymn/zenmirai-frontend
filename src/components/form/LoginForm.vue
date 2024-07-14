52e48fbe7b49e625330f9149f461bdd8b25faa4c
Roihan <hroihan6@gmail.com>
2024-07-14 14:36:02 +0700

    create login view

---
+++ src/components/form/LoginForm.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import CInput from '@/components/ui/CInput.vue'
+import CButton from '@/components/ui/CButton.vue'
+</script>
+
+<template>
+  <form class="flex flex-col gap-y-4">
+    <CInput id="username" label="Username" />
+    <CInput id="password" label="Password" />
+    <CButton>Sign Up</CButton>
+    <p class="self-end">
+      Don't have an account?
+      <a class="text-blue-700 underline underline-offset-2 cursor-pointer">Register</a>
+    </p>
+  </form>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import LoginForm from '@/components/form/LoginForm.vue'
+</script>
+
+<template>
+  <main class="flex flex-col mx-4 my-4 gap-y-4 lg:flex-row-reverse items-center">
+    <div id="img-container" class="w-full h-full lg:w-[50%]">
+      <img
+        src="https://images.unsplash.com/photo-1700211436013-ee7494931b42?q=80&w=1933&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="flower paints"
+        class="h-[180px] w-[100%] rounded-lg object-cover lg:h-[96vh]"
+      />
+    </div>
+    <div id="content" class="flex flex-col w-full lg:w-[50%]">
+      <div class="lg:w-[50%] lg:self-center flex flex-col gap-y-4">
+        <div id="intro" class="flex flex-col gap-y-2">
+          <h1 class="text-[24px] font-semibold">Welcome Back 👋</h1>
+          <p class="text-[#313957] text-[15px]">
+            Today is a new day. It's your day. You shape it. Sign in to start managing your projects
+          </p>
+        </div>
+        <LoginForm />
+      </div>
+    </div>
+  </main>
+</template>
